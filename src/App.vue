57994e79a80d6d9d0aaa921504971260c69709bf
hyunsungJoo <joo@LAPTOP-M7ISC7UR.>
2024-01-31 14:48:24 +0900

    vue json 상하차 해보기
    - [ ] json코드 v-for를 이용하여 불러오기

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import category from '@/assets/category.json'
 
-const parentMessage = ref('Parent')
-const items = ref([{ message: 'Foo' }, { message: 'Bar'}])
+const categories = category
 </script>
 
 <template>
-	<li v-for="(item, index) in items">
-		{{ parentMessage }} - {{ index }} - {{ item.message }}
+	<li v-for="obj in categories">
+		{{obj.name}} - {{obj.image_url}} - {{obj.code}}
 	</li>
 </template>
