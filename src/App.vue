87bb2785f2aabb0080e4ff7f4c2cd94b1be454bf
hyunsungJoo <joo@LAPTOP-M7ISC7UR.>
2024-02-02 10:09:57 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import Child from './components/Child.vue'
 
-let id = 0;
-const newTodo = ref('')
-const todos = ref([
-	{ id: id ++, issue: 'Backlog' }, { id: id++, issue: 'Todo' },
-	{ id: id++, issue: 'Going Hawaii' }
-])
-
-function addTodo() {
-        todos.value.push({ id: id++ , issue: newTodo.value })
-	newTodo.vale = ''
-}
-
-function removeTodo(todo) {
-        todos.value = todos.value.filter(t => t !== todo)
-}
-
+const pocketMoney = ref('부모 컴포넌트로부터 <.✉️>을 전달받았어요!')
 </script>
 
 <template>
-        <form @submit.prevent="addTodo">
-                <input v-model="newTodo">
-                <button>할 일 추가</button>
-        </form>
-        <ul>
-                <li v-for="todo in todos">
-                        {{ todo.issue }} - <button @click="removeTodo(todo)">Done</button>
-                </li>
-        </ul>
+	<Child :pocketMoney="pocketMoney" />
 </template>
-
